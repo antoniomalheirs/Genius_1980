d5da82baa50db3625dc3401b0e7e9da08fea0c7d
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-13 22:46:24 -0300

    base/layout

---
+++ app/src/main/java/com/example/geniusv02/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.geniusv02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnjogar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnjogar = findViewById(R.id.btnjogar);
+
+        btnjogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), telajogo.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
